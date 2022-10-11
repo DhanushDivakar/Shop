2c4ca9ed3af54824fe558ba8766288f981c538db
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-10-11 20:01:02 +0530

    validator

--- lib/screens/edit_products_screen.dart
+++ lib/screens/edit_products_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../providers/product.dart';
+
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
 
@@ -13,10 +15,16 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _imageUrlController = TextEditingController();
   final _imageUrlFocusNode = FocusNode();
 
+  final _form = GlobalKey<
+      FormState>(); //this is needed when we need to interrecat with the widget isnisde code, we also need ot mention which kind of data does it mention to
+  var _editedProduct =
+      Product(id: null, title: '', price: 0, description: '', imageUrl: '');
+
   @override
   void initState() {
     _imageUrlController.addListener(_updateImageUrl);
   }
+
   @override
   void dispose() {
     _imageUrlController.removeListener(_updateImageUrl);
@@ -24,53 +32,123 @@ class _EditProductScreenState extends State<EditProductScreen> {
     _descriptionFocusNode.dispose();
     _imageUrlController.dispose();
     _imageUrlFocusNode.dispose();
-    
+
     super.dispose();
   }
+
   void _updateImageUrl() {
-    if(!_imageUrlFocusNode.hasFocus){
+    if (!_imageUrlFocusNode.hasFocus) {
       setState(() {});
     }
-
   }
 
+  void _saveform() {
+    final isValid = _form.currentState.validate();
+    if(!isValid) {
+      return;//this will stop the exceution of the code
+    }
+    _form.currentState.save();
+    print(_editedProduct.title);
+    print(_editedProduct.description);
+    print(_editedProduct.price);
+    print(_editedProduct.imageUrl);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
+        actions: [
+          IconButton(
+            onPressed: _saveform, //this points to the method
+            icon: Icon(Icons.save),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Form(
+          key: _form,
           child: ListView(
             children: [
               TextFormField(
                 decoration: InputDecoration(
+                  //we can decorate eroor messgaes by calling its methods
                   labelText: 'Title',
                 ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Please provide a value";
+                  }
+                  return null;
+                },
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(
                       _priceFocusNode); //when the nxt button is pressend on the soft keyboard then th focus will shift to the nxt textformfield
+                },
+                onSaved: (value) {
+                  _editedProduct = Product(
+                      title: value,
+                      price: _editedProduct.price,
+                      description: _editedProduct.description,
+                      imageUrl: _editedProduct.imageUrl,
+                      id: null);
                 },
               ),
               TextFormField(
                 decoration: InputDecoration(
                   labelText: 'Price',
                 ),
+                validator: (value){
+                  if(value.isEmpty){
+                    return 'Please enter a price';
+                  }
+                  if(double.tryParse(value) == null){//this will not return if the value is null
+                    return 'Please enter valid number';
+                  }
+                  if(double.parse(value) <= 0){
+                    return 'please a number greater than 0';
+                  }
+                },
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
                 focusNode: _priceFocusNode,
+                onSaved: (value) {
+                  _editedProduct = Product(
+                      title: _editedProduct.title,
+                      price: double.parse(value),
+                      description: _editedProduct.description,
+                      imageUrl: _editedProduct.imageUrl,
+                      id: null);
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(
                   labelText: 'Description',
                 ),
+                validator: (value){
+                  if(value.isEmpty){
+                    return'please enter a description';
+                  }
+                  if(value.length < 10){
+                    return 'Should be at least 10 characters long';
+                  }
+                  return null;
+
+                },
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
+                onSaved: (value) {
+                  _editedProduct = Product(
+                      title: _editedProduct.title,
+                      price: _editedProduct.price,
+                      description: value,
+                      imageUrl: _editedProduct.imageUrl,
+                      id: null);
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -98,6 +176,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
                       textInputAction: TextInputAction.done,
                       controller: _imageUrlController,
                       focusNode: _imageUrlFocusNode,
+                      onFieldSubmitted: (_) {
+                        _saveform();
+                      },
+                      onSaved: (value) {
+                        _editedProduct = Product(
+                            title: _editedProduct.title,
+                            price: _editedProduct.price,
+                            description: _editedProduct.description,
+                            imageUrl: value,
+                            id: null);
+                      },
                     ),
                   ),
                 ],
