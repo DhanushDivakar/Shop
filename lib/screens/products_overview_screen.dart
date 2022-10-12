6286fb42ee1684dee582d6f8fe8afa4e969a9839
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-10-12 19:37:34 +0530

    validator

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -20,6 +20,35 @@ class ProductOverviewScreen extends StatefulWidget {
 
 class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
   var _showOnlyFavorites = false;
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    //Provider.of<Products>(context).fetchAndSetProducts(); //this wont work in initstate bcz in initstate context wont work
+    // Future.delayed(Duration.zero).then((_) {                 //
+    //   Provider.of<Products>(context).fetchAndSetProducts();  // this is bit of a hack which works btw
+    // });                                                      //
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    //this will run after the widegt as been fully initialized but this will run often
+    if (_isInit) {
+      //this code runs when the page first loads
+      setState(() {
+        _isLoading = true;
+      });
+
+      Provider.of<Products>(context).fetchAndSetProducts().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,8 +77,8 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
               PopupMenuItem(child: Text("Show All"), value: FilterOption.All)
             ],
           ),
-          Consumer<Cart>(builder: (_, cart, ch) =>
-             Badge(
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
               child: ch,
               value: cart.itemCount.toString(),
             ),
@@ -63,7 +92,11 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
         ],
       ),
       drawer: AppDrawer(),
-      body: ProductsGrid(_showOnlyFavorites),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ProductsGrid(_showOnlyFavorites),
     );
   }
 }
