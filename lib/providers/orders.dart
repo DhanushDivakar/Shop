6ddf165cf18b2e1f6ebfbe8dcc1d9ac8e1621ae7
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-10-12 22:34:19 +0530

    cart edited

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -25,9 +25,39 @@ class Orders with ChangeNotifier {
     ]; //we are addig outside the class because we can't edit orders
   }
 
+  Future<void> fetchAndSetOrders() async {
+    const url =
+        'https://shop-app-7a6f0-default-rtdb.firebaseio.com/Orders.json';
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if(extractedData == null){
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(OrderItem(
+        id: orderId,
+        amount: orderData['amount'],
+        dateTime: DateTime.parse(orderData['dateTime']),
+        products: (orderData['products'] as List<dynamic>)
+            .map(
+              (item) => CartItem(
+                id: item['id'],
+                title: item['title'],
+                quantity: item['quantity'],
+                price: item['price'],
+              ),
+            )
+            .toList(),
+      ));
+    });
+    _orders = loadedOrders;
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     const url =
-        'https://shop-app-7a6f0-default-rtdb.firebaseio.com/orders.json';
+        'https://shop-app-7a6f0-default-rtdb.firebaseio.com/Orders.json';
     final timestamp = DateTime.now();
     final response = await http.post(
       url,
