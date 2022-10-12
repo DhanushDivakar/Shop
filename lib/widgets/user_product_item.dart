e06f441391166bb26a203b6d70d7e95c3d62205e
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-10-12 20:42:42 +0530

    validator

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -9,10 +9,11 @@ class UserProductItem extends StatelessWidget {
   final String title;
   final String imageUrl;
 
-  UserProductItem(this.id,this.title, this.imageUrl);
+  UserProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
+    final scaffold = Scaffold.of(context);
     return ListTile(
       title: Text(title),
       leading: CircleAvatar(
@@ -24,14 +25,20 @@ class UserProductItem extends StatelessWidget {
           children: [
             IconButton(
               onPressed: () {
-                Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments: id);
+                Navigator.of(context)
+                    .pushNamed(EditProductScreen.routeName, arguments: id);
               },
               icon: Icon(Icons.edit),
               color: Theme.of(context).primaryColor,
             ),
             IconButton(
-              onPressed: () {
-                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              onPressed: () async {
+                try {
+                  await Provider.of<Products>(context, listen: false)
+                      .deleteProduct(id);
+                } catch (error) {
+                  scaffold.showSnackBar(SnackBar(content: Text('Deleting failed')));
+                }
               },
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
