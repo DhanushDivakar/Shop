ff8eb8249a2dfa805c92f09a51fa742baed6176a
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-10-14 20:08:26 +0530

    cart edited

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -28,8 +28,8 @@ class Orders with ChangeNotifier {
   }
 
   Future<void> fetchAndSetOrders() async {
-    const url =
-        'https://shop-app-7a6f0-default-rtdb.firebaseio.com/Orders.json';
+    final url =
+        'https://shop-app-7a6f0-default-rtdb.firebaseio.com/Orders.json?auth=$authToken';
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -58,8 +58,8 @@ class Orders with ChangeNotifier {
   }
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    const url =
-        'https://shop-app-7a6f0-default-rtdb.firebaseio.com/Orders.json';
+    final url =
+        'https://shop-app-7a6f0-default-rtdb.firebaseio.com/Orders.json?auth=$authToken';
     final timestamp = DateTime.now();
     final response = await http.post(
       url,
